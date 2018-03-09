b9c83a9da87af949395b2632fb382bd83ad245a6
pokotsun <gorigolila@gmail.com>
2018-03-09 23:26:05 +0900

    Tabを表示できるようにした

--- app/src/main/java/com/gorigolilagmail/kyutechapp2018/MainActivity.kt
+++ app/src/main/java/com/gorigolilagmail/kyutechapp2018/MainActivity.kt
@@ -2,11 +2,21 @@ package com.gorigolilagmail.kyutechapp2018
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tabs = arrayOf(
+                tab_layout.newTab(),
+                tab_layout.newTab(),
+                tab_layout.newTab())
+        tabs.forEachIndexed { i, v ->
+            v.text = "タブ$i"
+            tab_layout.addTab(v)
+        }
     }
 }
