81759dca36c9289ff3ecfb6200cec9555e70a982
pokotsun <gorigolila@gmail.com>
2018-05-31 12:22:12 +0900

    LoginActivityの見た目修正

---
+++ app/src/main/java/com/gorigolilagmail/kyutechapp2018/view/activity/LoginActivity.kt
@@ -0,0 +1,132 @@
+package com.gorigolilagmail.kyutechapp2018.view.activity
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.annotation.TargetApi
+import android.content.pm.PackageManager
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.app.LoaderManager.LoaderCallbacks
+import android.content.CursorLoader
+import android.content.Loader
+import android.database.Cursor
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Build
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.text.TextUtils
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+import java.util.ArrayList
+import android.Manifest.permission.READ_CONTACTS
+import com.gorigolilagmail.kyutechapp2018.R
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+//    private var mAuthTask: UserLoginTask? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        email_sign_in_button.setOnClickListener { attemptLogin() }
+    }
+
+
+    private fun attemptLogin() {
+
+        // Reset errors.
+        email.error = null
+        password.error = null
+
+        // Store values at the time of the login attempt.
+        val emailStr = email.text.toString()
+        val passwordStr = password.text.toString()
+
+        var cancel = false
+        var focusView: View? = null
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(passwordStr) && !isPasswordValid(passwordStr)) {
+            password.error = getString(R.string.error_invalid_password)
+            focusView = password
+            cancel = true
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(emailStr)) {
+            email.error = getString(R.string.error_field_required)
+            focusView = email
+            cancel = true
+        } else if (!isEmailValid(emailStr)) {
+            email.error = getString(R.string.error_invalid_email)
+            focusView = email
+            cancel = true
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView?.requestFocus()
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            showProgress(true)
+        }
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        //TODO: Replace this with your own logic
+        return email.contains("@")
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        //TODO: Replace this with your own logic
+        return password.length > 4
+    }
+
+    private fun showProgress(show: Boolean) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            val shortAnimTime = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
+
+            login_form.visibility = if (show) View.GONE else View.VISIBLE
+//            login_form.animate()
+//                    .setDuration(shortAnimTime)
+//                    .alpha((if (show) 0 else 1).toFloat())
+//                    .setListener(object : AnimatorListenerAdapter() {
+//                        override fun onAnimationEnd(animation: Animator) {
+////                            login_form.visibility = if (show) View.GONE else View.VISIBLE
+//                        }
+//                    })
+
+            login_progress.visibility = if (show) View.VISIBLE else View.GONE
+//            login_progress.animate()
+//                    .setDuration(shortAnimTime)
+//                    .alpha((if (show) 1 else 0).toFloat())
+//                    .setListener(object : AnimatorListenerAdapter() {
+//                        override fun onAnimationEnd(animation: Animator) {
+//                            login_progress.visibility = if (show) View.VISIBLE else View.GONE
+//                        }
+//                    })
+        } else { // API level < 23のとき
+            login_progress.visibility = if (show) View.VISIBLE else View.GONE
+            login_form.visibility = if (show) View.GONE else View.VISIBLE
+        }
+    }
+
+    
+    companion object {
+
+        /**
+         * Id to identity READ_CONTACTS permission request.
+         */
+        private val REQUEST_READ_CONTACTS = 0
+    }
+}
