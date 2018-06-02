6885fef3b977d5a790fde394369ec55815561c27
pokotsun <gorigolila@gmail.com>
2018-06-03 02:02:20 +0900

    各定数を追加

--- app/src/main/java/com/gorigolilagmail/kyutechapp2018/model/UserSchedule.kt
+++ app/src/main/java/com/gorigolilagmail/kyutechapp2018/model/UserSchedule.kt
@@ -16,6 +16,22 @@ data class UserSchedule(
                 day = day, period = period, quarter = quarter,
                 memo = "メモ", lateNum = 13, absentNum = 12
         )
-        
+
+        val FIRST_PERIOD = 0
+        val SECOND_PERIOD = 1
+        val THIRD_PERIOD = 2
+        val FOURTH_PERIOD = 3
+        val FIFTH_PERIOD = 4
+
+        val FIRST_QUARTER = 0
+        val SECOND_QUARTER = 1
+        val THIRD_QUARTER = 2
+        val FOURTH_QUARTER = 3
+
+        val MON_DAY = 0
+        val TUES_DAY = 1
+        val WEDNES_DAY = 2
+        val THURS_DAY = 3
+        val FRI_DAY = 4
     }
 }
